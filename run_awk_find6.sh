1c73d3a0571458ae60092d4e5d4b2948c51437b7
lidan <lidan@shilupan.com>
2019-04-28 22:35:41 +0800

    fix find6

--- run_awk_find6.sh
+++ run_awk_find6.sh
@@ -706,42 +706,42 @@ awk '/GCTCAAGCCAAGTTCCATATTA/{ print $0 }' new_data/large-library-TM2_FKDL190729
 awk '/GCTCAAGCCAAGTTCCATATTA/{ print $0 }' new_data/large-library-TM3_FKDL190729648-1a-11_1.clean.fq > rlt/GCTCAAGCCAAGTTCCATATTA.large-library-TM3_FKDL190729648-1a-11_1.clean.fq.txt
 awk '/GCTCAAGCCAAGTTCCATATTA/{ print $0 }' new_data/large-library-vitroF_FKDL190729648-1a-1_1.clean.fq > rlt/GCTCAAGCCAAGTTCCATATTA.large-library-vitroF_FKDL190729648-1a-1_1.clean.fq.txt
 awk '/GCTCAAGCCAAGTTCCATATTA/{ print $0 }' new_data/large-library-vitroM_FKDL190729648-1a-2_1.clean.fq > rlt/GCTCAAGCCAAGTTCCATATTA.large-library-vitroM_FKDL190729648-1a-2_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B11_FKDL190729648-1a-23_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B11_FKDL190729648-1a-23_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B12_FKDL190729648-1a-25_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B12_FKDL190729648-1a-25_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B13_FKDL190729648-1a-26_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B13_FKDL190729648-1a-26_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B21_FKDL190729648-1a-27_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B21_FKDL190729648-1a-27_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B22_FKDL190729648-1a-28_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B22_FKDL190729648-1a-28_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B23_FKDL190729648-1a-29_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B23_FKDL190729648-1a-29_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B31_FKDL190729648-1a-30_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B31_FKDL190729648-1a-30_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B32_FKDL190729648-1a-31_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B32_FKDL190729648-1a-31_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B33_FKDL190729648-1a-32_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B33_FKDL190729648-1a-32_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B41_FKDL190729648-1a-33_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B41_FKDL190729648-1a-33_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B42_FKDL190729648-1a-AK187_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B42_FKDL190729648-1a-AK187_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B43_FKDL190729648-1a-AK2086_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B43_FKDL190729648-1a-AK2086_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B51_FKDL190729648-1a-AK5274_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B51_FKDL190729648-1a-AK5274_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B52_FKDL190729648-1a-AK828_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B52_FKDL190729648-1a-AK828_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-B53_FKDL190729648-1a-AK829_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-B53_FKDL190729648-1a-AK829_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-GC1_FKDL190729648-1a-18_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-GC1_FKDL190729648-1a-18_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-GC2_FKDL190729648-1a-19_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-GC2_FKDL190729648-1a-19_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-GC3_FKDL190729648-1a-20_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-GC3_FKDL190729648-1a-20_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-GC4_FKDL190729648-1a-21_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-GC4_FKDL190729648-1a-21_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-GC5_FKDL190729648-1a-22_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-GC5_FKDL190729648-1a-22_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-NG1_FKDL190729648-1a-12_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-NG1_FKDL190729648-1a-12_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-NG2_FKDL190729648-1a-13_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-NG2_FKDL190729648-1a-13_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-NG3_FKDL190729648-1a-14_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-NG3_FKDL190729648-1a-14_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-NG4_FKDL190729648-1a-15_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-NG4_FKDL190729648-1a-15_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-NG5_FKDL190729648-1a-16_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-NG5_FKDL190729648-1a-16_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-TF11_FKDL190729648-1a-3_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-TF11_FKDL190729648-1a-3_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-TF12_FKDL190729648-1a-4_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-TF12_FKDL190729648-1a-4_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-TF13_FKDL190729648-1a-5_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-TF13_FKDL190729648-1a-5_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-TF21_FKDL190729648-1a-6_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-TF21_FKDL190729648-1a-6_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-TF22_FKDL190729648-1a-7_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-TF22_FKDL190729648-1a-7_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-TF23_FKDL190729648-1a-8_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-TF23_FKDL190729648-1a-8_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-TM1_FKDL190729648-1a-9_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-TM1_FKDL190729648-1a-9_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-TM2_FKDL190729648-1a-10_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-TM2_FKDL190729648-1a-10_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-TM3_FKDL190729648-1a-11_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-TM3_FKDL190729648-1a-11_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-vitroF_FKDL190729648-1a-1_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-vitroF_FKDL190729648-1a-1_1.clean.fq.txt
-awk '/GCTACTCCTGCGGGACTAAATT /{ print $0 }' new_data/large-library-vitroM_FKDL190729648-1a-2_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT .large-library-vitroM_FKDL190729648-1a-2_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B11_FKDL190729648-1a-23_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B11_FKDL190729648-1a-23_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B12_FKDL190729648-1a-25_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B12_FKDL190729648-1a-25_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B13_FKDL190729648-1a-26_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B13_FKDL190729648-1a-26_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B21_FKDL190729648-1a-27_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B21_FKDL190729648-1a-27_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B22_FKDL190729648-1a-28_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B22_FKDL190729648-1a-28_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B23_FKDL190729648-1a-29_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B23_FKDL190729648-1a-29_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B31_FKDL190729648-1a-30_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B31_FKDL190729648-1a-30_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B32_FKDL190729648-1a-31_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B32_FKDL190729648-1a-31_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B33_FKDL190729648-1a-32_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B33_FKDL190729648-1a-32_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B41_FKDL190729648-1a-33_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B41_FKDL190729648-1a-33_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B42_FKDL190729648-1a-AK187_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B42_FKDL190729648-1a-AK187_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B43_FKDL190729648-1a-AK2086_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B43_FKDL190729648-1a-AK2086_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B51_FKDL190729648-1a-AK5274_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B51_FKDL190729648-1a-AK5274_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B52_FKDL190729648-1a-AK828_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B52_FKDL190729648-1a-AK828_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-B53_FKDL190729648-1a-AK829_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-B53_FKDL190729648-1a-AK829_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-GC1_FKDL190729648-1a-18_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-GC1_FKDL190729648-1a-18_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-GC2_FKDL190729648-1a-19_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-GC2_FKDL190729648-1a-19_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-GC3_FKDL190729648-1a-20_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-GC3_FKDL190729648-1a-20_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-GC4_FKDL190729648-1a-21_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-GC4_FKDL190729648-1a-21_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-GC5_FKDL190729648-1a-22_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-GC5_FKDL190729648-1a-22_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-NG1_FKDL190729648-1a-12_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-NG1_FKDL190729648-1a-12_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-NG2_FKDL190729648-1a-13_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-NG2_FKDL190729648-1a-13_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-NG3_FKDL190729648-1a-14_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-NG3_FKDL190729648-1a-14_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-NG4_FKDL190729648-1a-15_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-NG4_FKDL190729648-1a-15_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-NG5_FKDL190729648-1a-16_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-NG5_FKDL190729648-1a-16_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-TF11_FKDL190729648-1a-3_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-TF11_FKDL190729648-1a-3_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-TF12_FKDL190729648-1a-4_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-TF12_FKDL190729648-1a-4_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-TF13_FKDL190729648-1a-5_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-TF13_FKDL190729648-1a-5_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-TF21_FKDL190729648-1a-6_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-TF21_FKDL190729648-1a-6_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-TF22_FKDL190729648-1a-7_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-TF22_FKDL190729648-1a-7_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-TF23_FKDL190729648-1a-8_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-TF23_FKDL190729648-1a-8_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-TM1_FKDL190729648-1a-9_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-TM1_FKDL190729648-1a-9_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-TM2_FKDL190729648-1a-10_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-TM2_FKDL190729648-1a-10_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-TM3_FKDL190729648-1a-11_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-TM3_FKDL190729648-1a-11_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-vitroF_FKDL190729648-1a-1_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-vitroF_FKDL190729648-1a-1_1.clean.fq.txt
+awk '/GCTACTCCTGCGGGACTAAATT/{ print $0 }' new_data/large-library-vitroM_FKDL190729648-1a-2_1.clean.fq > rlt/GCTACTCCTGCGGGACTAAATT.large-library-vitroM_FKDL190729648-1a-2_1.clean.fq.txt
 awk '/GCATTGACTATGAACTCATTA/{ print $0 }' new_data/large-library-B11_FKDL190729648-1a-23_1.clean.fq > rlt/GCATTGACTATGAACTCATTA.large-library-B11_FKDL190729648-1a-23_1.clean.fq.txt
 awk '/GCATTGACTATGAACTCATTA/{ print $0 }' new_data/large-library-B12_FKDL190729648-1a-25_1.clean.fq > rlt/GCATTGACTATGAACTCATTA.large-library-B12_FKDL190729648-1a-25_1.clean.fq.txt
 awk '/GCATTGACTATGAACTCATTA/{ print $0 }' new_data/large-library-B13_FKDL190729648-1a-26_1.clean.fq > rlt/GCATTGACTATGAACTCATTA.large-library-B13_FKDL190729648-1a-26_1.clean.fq.txt
